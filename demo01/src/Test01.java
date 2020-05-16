44760964973ab8dec445bec45287863f953f6bd9
zsw <2323493244@qq.com>
2020-05-16 22:11:33 +0800

    4

--- demo01/src/Test01.java
+++ demo01/src/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
     public static void main(String[] args) {
         Map<String,String> map=new HashMap<>();
         map.put("z","zz");
+        map.put("s","ss");
         System.out.println(map);
     }
 }
