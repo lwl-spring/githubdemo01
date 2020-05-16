5e026b3b1585a0f5a56d6db33929ca1ebfee3ce9
zsw <2323493244@qq.com>
2020-05-16 23:19:10 +0800

    10

--- demo01/src/Test01.java
+++ demo01/src/Test01.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Test01 {
     public static void main(String[] args) {
+        Map set=Collections.synchronizedMap(new HashMap<>());
+        System.out.println("a");
         HashSet set=new HashSet();
     }
 }
