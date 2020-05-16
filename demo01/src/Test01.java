baef387fa624bda54df094c71e5684891780b12c
liweilong <1225478409@qq.com>
2020-05-16 21:52:38 +0800

    1

---
+++ demo01/src/Test01.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Test01 {
+    public static void main(String[] args) {
+        HashMap<String,String> map=new HashMap<>();
+        map.put("z","zz");
+        System.out.println(map);
+    }
+}
