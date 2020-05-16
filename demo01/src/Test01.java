860c2e2003ead9d8c22e100c4b6a1e6090ba23c3
zsw <2323493244@qq.com>
2020-05-16 23:12:54 +0800

    9

--- demo01/src/Test01.java
+++ demo01/src/Test01.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 public class Test01 {
     public static void main(String[] args) {
+        HashSet set=new HashSet();
         System.out.println("a");
     }
 }
