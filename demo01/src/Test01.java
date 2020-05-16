ca9853cf89af7280c75f4fcb233d721ea696fb7c
liweilong <1225478409@qq.com>
2020-05-16 23:16:26 +0800

    9

--- demo01/src/Test01.java
+++ demo01/src/Test01.java
@@ -3,10 +3,5 @@ import java.util.*;
 public class Test01 {
     public static void main(String[] args) {
         HashSet set=new HashSet();
-        List list=new Vector();
-        list.add("a");
-        list.add("b");
-        System.out.println(list);
-        System.out.println("a");
     }
 }
