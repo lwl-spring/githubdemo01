5144b8b79f818ab0c2fe098a167f16f4651c3d43
liweilong <1225478409@qq.com>
2020-05-16 22:41:19 +0800

    5

--- demo01/src/Test01.java
+++ demo01/src/Test01.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 public class Test01 {
     public static void main(String[] args) {
-        Map<String,String> map=new HashMap<>();
-        map.put("z","zz");
-        System.out.println(map);
+        List list=new ArrayList<>();
+        list.add("a");
+        System.out.println(list);
     }
 }
